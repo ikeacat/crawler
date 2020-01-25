ddd318bc09f741dffa89dfe4da530a9dcff58700
ikeacat <mfryk268@gmail.com>
2020-01-25 21:37:29 +0000

    Dark mode & switcher

--- dynamic.js
+++ dynamic.js
@@ -1,22 +1,26 @@
 function loadStartMenu() {
     document.title = "Index of /crawler"
     document.getElementById("indextitle").innerHTML = "Index of /crawler";
-    document.getElementById("indexlist").innerHTML = `<ul id='insertLater'>
-    <li>/startNewGame</li>
-    <li>/continueOldGame</li>
-    <ul>`;
     var darkSetting = localStorage.getItem("darkSetting")
     if(darkSetting == "dark") {
-        console.log("Found Light mode.")
-        document.getElementById("insertLater").innerHTML += "<li>activateLightMode.exe</li>"
-    } else if(darkSetting == "light") {
         console.log("Found Dark mode.")
-        document.getElementById("insertLater").innerHTML += "<li><a>activateDarkMode.exe</a></li>"
+        document.getElementById("csslink").setAttribute("href","darkMode.css");
+        var dm = `<li><a id='dmswitch' href='javascript:darkMode("light");'>switchToLightMode.exe</a></li>`
+    } else if(darkSetting == "light") {
+        console.log("Found Light mode.")
+        document.getElementById("csslink").setAttribute("href","lightMode.css");
+        var dm = `<li><a id='dmswitch' href='javascript:darkMode("dark");'>switchToDarkMode.exe</a></li>`
     } else {
         console.log("No darkSetting thing, assuming light.")
-        document.getElementById("insertLater").innerHTML += "<li>activateDarkMode.exe</li>"
-        localStorage.setItem("darkSetting","light")
+        var dm = `<li><a id='dmswitch' href='javascript:darkMode("dark");'>switchToDarkMode.exe</a></li>`
+        document.getElementById("csslink").setAttribute("href","lightMode.css");
+        localStorage.setItem("darkSetting","light");
     }
+    document.getElementById("indexlist").innerHTML = `<ul id='insertLater'>
+    <li>/startNewGame</li>
+    <li>/continueOldGame</li>` + dm +
+    `</ul>`;
+    
 }
 
 function darkMode(dms) {
@@ -24,8 +28,12 @@ function darkMode(dms) {
     if(dms == "dark") {
         localStorage.setItem("darkSetting","dark");
         document.getElementById("csslink").setAttribute("href","darkMode.css");
+        document.getElementById("dmswitch").innerHTML = 'switchToLightMode.exe';
+        document.getElementById("dmswitch").setAttribute("href","javascript:darkMode('light')");
     } else if (dms == "light") {
         localStorage.setItem("darkSetting","light");
         document.getElementById("csslink").setAttribute("href","lightMode.css");
+        document.getElementById("dmswitch").innerHTML = 'switchToDarkMode.exe';
+        document.getElementById("dmswitch").setAttribute("href","javascript:darkMode('dark')");
     }
 }
