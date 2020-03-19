303ada854eeabb80fb6697733913a87292cfd31b
ikeacat <mfryk268@gmail.com>
2020-03-18 21:34:30 -0500

    its wip.

--- dynamic.js
+++ dynamic.js
@@ -1,2 +1,10 @@
 var versionNumber = "1.2"
 
+function startGame() {
+  var i;
+  var attr;
+  for(i = 0; i != -511; i--) {
+    attr = "transform: translateX(" + i + "px);";
+    document.getElementById("fullHS").setAttribute("style", attr)
+  }
+}
