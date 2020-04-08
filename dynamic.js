e70c19cf56af965097dbfd21b05646f120a980a0
ikeacat <mfryk268@gmail.com>
2020-04-08 08:36:58 -0500

    Maybe?

--- dynamic.js
+++ dynamic.js
@@ -1,10 +1,18 @@
 var versionNumber = "1.2"
 
 function startGame() {
-  var i;
-  var attr;
-  for(i = 0; i != -511; i--) {
-    attr = "transform: translateX(" + i + "px);";
-    document.getElementById("fullHS").setAttribute("style", attr)
-  }
+  document.getElementsByClassName("HSBody")[0].classList.remove("HSBody")
+  document.getElementById("rootDIV").innerHTML = `<button class='connectButton' onclick='naidsetup()'>Connect</button>`;
+}
+
+function naidsetup() {
+  document.getElementById("rootDIV").innerHTML = `<h1>Welcome to the National North American Investigative Department! (NNAID)</h1>
+  <p>To get started, please enter a password and name.</p><br>
+  <h5>Name: </h5>
+  <input id='un' />
+  <br>
+  <h5>Password: </h5>
+  <input id='pw' />
+  <br><br><br>
+  <button class='defbutton noButtonGoUp'>Submit to NNAID</button>`
 }
