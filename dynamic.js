e83b96aebae1feff96d4cc4eef023327bcdb980d
ikeacat <mfryk268@gmail.com>
2020-04-08 11:07:47 -0500

    Some Home Screen building.

--- dynamic.js
+++ dynamic.js
@@ -1,7 +1,7 @@
 var versionNumber = "1.2"
 
 function startGame() {
-  document.getElementsByClassName("HSBody")[0].classList.remove("HSBody")
+  document.getElementsByClassName("HSBody")[0].classList.remove("HSBody");
   document.getElementById("rootDIV").innerHTML = `<button class='connectButton' onclick='naidsetup()'>Connect</button>`;
 }
 
@@ -14,5 +14,30 @@ function naidsetup() {
   <h5>Password: </h5>
   <input id='pw' />
   <br><br><br>
-  <button class='defbutton noButtonGoUp'>Submit to NNAID</button>`
+  <button class='defbutton noButtonGoUp' onclick='submitCreds()'>Submit to NNAID</button>`;
+}
+
+function submitCreds() {
+  localStorage.clear()
+  var usrnm = document.getElementById("un").value;
+  var paswd = document.getElementById("pw").value;
+  localStorage.setItem("username", usrnm);
+  localStorage.setItem("password", paswd);
+  messageFromDirector();
+}
+
+function messageFromDirector() {
+  var usrnm = localStorage.getItem("username");
+  document.getElementById("rootDIV").innerHTML = `<h1>Message from the Director</h1>
+  <p>Hello, ` + usrnm + `,</p>
+  <p>Welcome to the NNAID! I am glad to have you join us.<br>
+  I will let you get yourself situated.<br>
+  Once you are done, go to the missions page and select the 'Introduction' mission.<br></p>
+  <button class='defbutton' onclick='homeScreen()'>Continue to Home Screen</button>`;
+}
+
+function homeScreen() {
+  document.getElementById("rootDIV").innerHTML = `<div><div id='infoCard' class='defaultCardDynamic HSInfoCard'><h1 class='leftfifty welcomeMessage'>Welcome, <span id='un'></span>!</div>
+  <div class='defaultCardDynamic HSCardTopMargin HSMissionCard' style='height: 250px;'><h1 class='leftfifty' style='top:140px;'>Missions</h1></div></div>`;
+  document.getElementById("un").innerHTML = localStorage.getItem("username");
 }
