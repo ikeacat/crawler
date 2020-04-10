962708859d7bdfb457597e8729cf35b3e2feb996
ikeacat <mfryk268@gmail.com>
2020-04-10 14:29:51 -0500

    Settings page & Allow rainbow switch off.

--- dynamic.js
+++ dynamic.js
@@ -26,6 +26,7 @@ function submitCreds() {
   }
   localStorage.setItem("username", usrnm);
   localStorage.setItem("password", paswd);
+  localStorage.setItem("rainbow","true");
   messageFromDirector();
 }
 
@@ -40,8 +41,51 @@ function messageFromDirector() {
 }
 
 function homeScreen() {
+  var rainbow = localStorage.getItem('rainbow');
   document.getElementById("rootDIV").innerHTML = `<div><div id='infoCard' class='defaultCardDynamic HSInfoCard'><h1 class='leftfifty welcomeMessage'>Welcome, <span id='un'></span>!</div>
-  <div class='defaultCardDynamic HSCardTopMargin HSMissionCard' style='height: 250px;'><h1 class='leftfifty HSCardText'>Missions</h1></div>
-  <div class='defaultCardDynamic HSCardTopMargin HSSettingsCard'><h1 class='leftfifty HSCardText'>Settings</h1></div></div>`;
+  <div class='defaultCardDynamic HSCardTopMargin HSMissionCard' id='MissionCard' style='height: 250px;'><h1 class='leftfifty HSCardText'>Missions</h1></div>
+  <a href='javascript:settingsPage()' class='noAUnderline'><div class='defaultCardDynamic HSCardTopMargin HSSettingsCard' id='SettingCard'><h1 class='leftfifty HSCardText'>Settings</h1></div></a></div>`;
   document.getElementById("un").innerHTML = localStorage.getItem("username");
+  if(rainbow == "false") {
+    document.getElementById("MissionCard").classList.add("altHSBGAni");
+    document.getElementById("SettingCard").classList.add("altHSBGAni");
+  }
+}
+
+function settingsPage() {
+  document.getElementById("rootDIV").innerHTML = `<div id='settingsCard' class='defaultCardDynamic HSInfoCard'><div class='leftfifty'>
+  <h1 style='font-size:60px;'>Settings</h1>
+  <br>
+  <h2>Home Screen</h2>
+  <p>Rainbow animations around boxes</p>
+  <input type="radio" id="yesrainbow" name="rainbowanihsbox" value="yes">
+  <label for="yesrainbow">Have the rainbow</label><br>
+  <input type="radio" id="norainbow" name="rainbowanihsbox" value="no">
+  <label for="norainbow">No Rainbow (green)</label><br><br>
+  <button class='noButtonGoUp defbutton' onclick='saveSettings()'>Save</button>
+  <br><br><br>
+  </div></div>`
+  checkTrueValues();
+}
+
+function checkTrueValues() {
+  var rainbowVal = localStorage.getItem("rainbow");
+  if(rainbowVal == "true") {
+    document.getElementById("yesrainbow").checked = true;
+  } else if(rainbowVal == "false") {
+    document.getElementById("norainbow").checked = true;
+  } else { // Go To default value.
+    localStorage.setItem("rainbow","true");
+    checkTrueValues();
+  }
+}
+
+function saveSettings() {
+  var rainbowVals = document.getElementsByName("rainbowanihsbox");
+  if(rainbowVals[0].checked == true) {
+    localStorage.setItem("rainbow","true")
+  } else if (rainbowVals[1].checked == true) {
+    localStorage.setItem("rainbow","false")
+  }
+  homeScreen();
 }
